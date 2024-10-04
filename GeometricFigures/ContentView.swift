ead3fa3462d97e953a74e14a0243c7288133ac81
Elaine Fang <efang@lcs.on.ca>
2024-10-04 19:51:37 -0400

    Initial Commit

---
+++ GeometricFigures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeometricFigures
+//
+//  Created by Fang Elaine on 2024/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Fang Elaine on 2024/10/4.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
